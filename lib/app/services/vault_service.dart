03c7639ed79ef08bb6a41e012f67dc7aff1b7d81
anpigon <markan82@gmail.com>
2021-06-02 20:49:31 +0900

    valut service

---
+++ lib/app/services/vault_service.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get/state_manager.dart';
+
+import '../../logger.dart';
+
+class ValutService extends GetxService {
+  late final FlutterSecureStorage _secureStorage;
+
+  Future<ValutService> init() async {
+    logger.d('ValutService.init');
+    _secureStorage = FlutterSecureStorage();
+    return this;
+  }
+
+  Future<String> write(String key, String value) async {
+    await _secureStorage.write(key: key, value: value);
+    return value;
+  }
+
+  Future<String?> read(String key, {String? defaultValue}) async {
+    return await _secureStorage.read(key: key) ?? defaultValue;
+  }
+}
