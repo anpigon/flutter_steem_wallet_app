d874077301f99761e991e2ae8b85e45fa1b2bffa
anpigon <markan82@gmail.com>
2021-06-06 15:12:52 +0900

    fix accountDropdownBox

--- lib/app/modules/wallets/views/wallets_view.dart
+++ lib/app/modules/wallets/views/wallets_view.dart
@@ -283,42 +283,64 @@ class WalletsView extends StatelessWidget {
     required List<String> items,
   }) {
     return Obx(
-      () => DropdownButton(
-        onChanged: onChanged,
-        value: value,
-        items: items
-            .map<DropdownMenuItem<String>>(
-              (username) => DropdownMenuItem(
-                value: username,
-                child: Row(children: [
-                  ClipRRect(
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(100),
-                    ),
-                    child: ColoredBox(
-                      color: Colors.white,
-                      child: Image.network(
-                        'https://steemitimages.com/u/$username/avatar',
-                        fit: BoxFit.cover,
-                        width: 36,
-                        height: 36,
-                        errorBuilder: (_, __, ___) => const Icon(
-                            Icons.account_circle,
-                            color: Colors.grey),
+      () => Container(
+        decoration: BoxDecoration(
+          color: Get.theme.buttonColor.withOpacity(0.1),
+          borderRadius: const BorderRadius.all(Radius.circular(50)),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 7),
+        // constraints: const BoxConstraints(minWidth: 100, maxWidth: 180),
+        child: DropdownButton(
+          onChanged: onChanged,
+          value: value,
+          items: items
+              .map<DropdownMenuItem<String>>(
+                (username) => DropdownMenuItem(
+                  value: username,
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(18),
+                        ),
+                        child: ColoredBox(
+                          color: Colors.white,
+                          child: Image.network(
+                            'https://steemitimages.com/u/$username/avatar',
+                            fit: BoxFit.cover,
+                            width: 36,
+                            height: 36,
+                            errorBuilder: (_, __, ___) => const Icon(
+                                Icons.account_circle,
+                                size: 36,
+                                color: Colors.grey),
+                          ),
+                        ),
                       ),
-                    ),
+                      const SizedBox(width: 10),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minWidth: 100,
+                          // username이 매우 긴 계정 때문에...
+                          maxWidth: (Get.width - (30 + 36 + 40 + (48 * 4))),
+                        ),
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown,
+                          alignment: Alignment.centerLeft,
+                          child: Text(username),
+                        ),
+                      ),
+                    ],
                   ),
-                  const SizedBox(width: 10),
-                  Text(username),
-                ]),
-              ),
-            )
-            .toList(),
-        underline: Container(),
-        icon: const Icon(Icons.expand_more),
-        style: Get.theme.textTheme.subtitle1!.copyWith(color: Colors.white),
-        iconEnabledColor: Colors.white,
-        dropdownColor: Get.theme.primaryColor,
+                ),
+              )
+              .toList(),
+          underline: Container(),
+          icon: const Icon(Icons.expand_more),
+          style: Get.theme.textTheme.subtitle1!.copyWith(color: Colors.white),
+          iconEnabledColor: Colors.white,
+          dropdownColor: Get.theme.primaryColor,
+        ),
       ),
     );
   }
