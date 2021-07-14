77a56e2f7091ad06cd06f8d7b4d3814d65710b08
jeongsk <jeiongseok@wantedlab.com>
2021-07-14 22:06:53 +0900

    Refactoring SteamProvider class

--- lib/app/data/steem_provider.dart
+++ lib/app/data/steem_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_steem_wallet_app/app/exceptions/http_exception.dart';
 import 'package:flutter_steem_wallet_app/app/models/account_history.dart';
 import 'package:flutter_steem_wallet_app/app/models/steem/steem_post.dart';
 import 'package:get/get.dart';
@@ -8,117 +9,105 @@ class SteemProvider extends GetConnect {
   static SteemProvider to = Get.find<SteemProvider>();
 
   int id = 0;
-
-  Future getAccountHistory(String account) async {
+  Future _request(String method, dynamic params) async {
+    print(method);
+    print(params);
     final body = {
+      'id': id++,
       'jsonrpc': '2.0',
-      'method': 'call',
-      'params': [
-        'database_api',
-        'get_state',
-        ['/@$account/transfers']
-      ]
+      'method': method,
+      'params': params
     };
     final response = await post('/', body);
-
-    if (response.statusCode == 200) {
-      final result = response.body['result'];
-      final data = result['accounts'][account];
-      final transferHistory = data['transfer_history'];
-      final otherHistory = data['other_history'];
-
-      // { "base": "0.323 SBD",  "quote": "1.000 STEEM" }
-      // final feed = result['feed_price'];
-      final props = result['props'];
-      final totalVestingFundSteem = props['total_vesting_fund_steem'] as String;
-      final totalVestingShares = props['total_vesting_shares'] as String;
-
-      final List<AccountHistory> accountHistory = transferHistory
-          .map<AccountHistory>((item) => AccountHistory.fromJson(
-                item,
-                ownerAccount: account,
-                totalVestingFundSteem: totalVestingFundSteem,
-                totalVestingShares: totalVestingShares,
-              ))
-          .toList();
-
-      accountHistory.addAll(otherHistory
-          .map<AccountHistory>((item) => AccountHistory.fromJson(
-                item,
-                ownerAccount: account,
-                totalVestingFundSteem: totalVestingFundSteem,
-                totalVestingShares: totalVestingShares,
-              ))
-          .toList());
-
-      accountHistory.sort((a, b) {
-        var adate = a.timestamp;
-        var bdate = b.timestamp;
-        return -adate.compareTo(bdate);
-      });
-
-      return accountHistory
-          .where(
-            (e) => e.message?.isNotEmpty ?? false,
-          )
-          .toList();
+    final url = response.request?.url.toString();
+    switch (response.statusCode) {
+      case 200:
+        final responseJson = response.body['result'];
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body, url);
+      case 401:
+      case 403:
+        throw UnAuthorizedException(response.body, url);
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error occured with code : ${response.statusCode}', url);
     }
-    return [];
+  }
+
+  Future getAccountHistory(String account) async {
+    final response = await _request('call', [
+      'database_api',
+      'get_state',
+      ['/@$account/transfers']
+    ]);
+
+    final data = response['accounts'][account];
+    final transferHistory = data['transfer_history'];
+    final otherHistory = data['other_history'];
+
+    final props = response['props'];
+    final totalVestingFundSteem = props['total_vesting_fund_steem'] as String;
+    final totalVestingShares = props['total_vesting_shares'] as String;
+
+    final List<AccountHistory> accountHistory = transferHistory
+        .map<AccountHistory>((item) => AccountHistory.fromJson(
+              item,
+              ownerAccount: account,
+              totalVestingFundSteem: totalVestingFundSteem,
+              totalVestingShares: totalVestingShares,
+            ))
+        .toList();
+
+    accountHistory.addAll(otherHistory
+        .map<AccountHistory>((item) => AccountHistory.fromJson(
+              item,
+              ownerAccount: account,
+              totalVestingFundSteem: totalVestingFundSteem,
+              totalVestingShares: totalVestingShares,
+            ))
+        .toList());
+
+    accountHistory.sort((a, b) {
+      var adate = a.timestamp;
+      var bdate = b.timestamp;
+      return -adate.compareTo(bdate);
+    });
+
+    return accountHistory
+        .where(
+          (e) => e.message?.isNotEmpty ?? false,
+        )
+        .toList();
   }
 
   /// 구독중인 커뮤니티 조회하기
   Future<List<SteemSubscription>> getAllSubscriptions(String account) async {
-    final body = {
-      'jsonrpc': '2.0',
-      'method': 'bridge.list_all_subscriptions',
-      'params': {'account': account}
-    };
-    final response = await post('/', body);
-    if (response.statusCode == 200) {
-      final result = response.body['result'];
-      final steemSubscriptions = result.map<SteemSubscription>((item) {
-        return SteemSubscription.fromJson(item);
-      }).toList();
-      return steemSubscriptions;
-    }
-    return [];
+    final response =
+        await _request('bridge.list_all_subscriptions', {'account': account});
+    return response
+        .map<SteemSubscription>((item) => SteemSubscription.fromJson(item))
+        .toList();
   }
 
   Future<List<SteemPost>> getMyFriendsFeeds(String account) async {
-    final body = {
-      'id': id++,
-      'jsonrpc': '2.0',
-      'method': 'bridge.get_account_posts',
-      'params': {'sort': 'feed', 'account': account, 'observer': account}
-    };
-    final response = await post('/', body);
-    if (response.statusCode == 200) {
-      final result = response.body['result'];
-      final List<SteemPost> steemPosts = result.map<SteemPost>((item) {
-        return SteemPost.fromJson(item);
-      }).toList();
-      return steemPosts;
-    }
-    return [];
+    final response = await _request('bridge.get_account_posts', {
+      'sort': 'feed',
+      'account': account,
+      'observer': account,
+    });
+    return response.map<SteemPost>((item) => SteemPost.fromJson(item)).toList();
   }
 
-  Future<List<SteemPost>> getCommunityFeeds(String tag, String account) async {
-    final body = {
-      'id': id++,
-      'jsonrpc': '2.0',
-      'method': 'bridge.get_ranked_posts',
-      'params': {'sort': 'trending', 'tag': tag, 'observer': account}
-    };
-    final response = await post('/', body);
-    if (response.statusCode == 200) {
-      final result = response.body['result'];
-      print(result);
-      final List<SteemPost> steemPosts = result.map<SteemPost>((item) {
-        return SteemPost.fromJson(item);
-      }).toList();
-      return steemPosts;
-    }
-    return [];
+  Future<List<SteemPost>> getCommunityFeeds(String tag, String account,
+      [String sort = 'trending']) async {
+    final List response = await _request('bridge.get_ranked_posts', {
+      'sort': sort,
+      'tag': tag,
+      'observer': account,
+    });
+    return response.map<SteemPost>((item) => SteemPost.fromJson(item)).toList();
   }
 
   @override
